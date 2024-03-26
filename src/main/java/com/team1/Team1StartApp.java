a94ab19387267878f9b18117678bc9b975cb0b9e
kulljam <adgrvet13@naver.com>
2024-03-26 18:33:14 +0900

    Initial commit

---
+++ src/main/java/com/team1/Team1StartApp.java
@@ -0,0 +1,16 @@
+package com.team1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team1StartApp {
+    public static void main(String[] args) {
+        SpringApplication.run( Team1StartApp.class );
+        }
+}
+// 최초 커밋
+
+
+
+
---
+++ src/main/java/com/team1/controller/Controller.java
@@ -0,0 +1,7 @@
+package com.team1.controller;
+
+public class Controller {
+
+
+
+}
