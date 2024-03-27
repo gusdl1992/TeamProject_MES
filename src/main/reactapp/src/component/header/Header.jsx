d4a9e4d07c9e30aed8a03c7ccf52dc03f0d21be3
me5290 <me5290@naver.com>
2024-03-27 17:59:22 +0900

    2024.03.27 이민형 레이아웃

--- src/main/reactapp/src/component/header/Header.jsx
+++ src/main/reactapp/src/component/header/Header.jsx
@@ -2,8 +2,8 @@ import styles from './header.css';
 
 export default function header(props){
     return (
-        <div>
-            <div id='headerWrap'>
+        <div id='menuWrap'>
+            <div id='globalNav'>
                 <div id='logoBox'>
                     로고
                 </div>
@@ -13,6 +13,25 @@ export default function header(props){
                 <div id='loginBox'>
                     로그인
                 </div>
+            </div>
+            <div id='sideNav'>
+                <ul>
+                    <li>
+                        자재관리
+                    </li>
+                    <li>
+                        생산관리
+                    </li>
+                    <li>
+                        로그
+                    </li>
+                    <li>
+                        경영통계
+                    </li>
+                    <li>
+                        관리자페이지
+                    </li>
+                </ul>
             </div>
         </div>
     )
