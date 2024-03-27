836f727d44fed42a521d90dcd087096ad26bb82f
me5290 <me5290@naver.com>
2024-03-27 17:40:27 +0900

    2024.03.27 이민형 레이아웃

--- src/main/reactapp/team1/src/content/Test.jsx
+++
@@ -1,7 +0,0 @@
-export default function Test(props){
-    return(
-        <div style={{maxWidth:'80%',minWidth:'1367px',height:'1800px',margin:'0 auto',border:'1px solid red'}}>
-
-        </div>
-    )
-}
