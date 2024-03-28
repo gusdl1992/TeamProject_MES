06bb2221a7721219f4bc70fb95731a02a3d8cf51
tlawo33 <tlawo33@naver.com>
2024-03-28 18:20:42 +0900

    심재훈커밋2

--- src/main/java/com/team1/model/repository/ProductRepository.java
+++ src/main/java/com/team1/model/repository/ProductRepository.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+
+
+
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity,Integer> {
     ProductEntity findBypname(String pname);
