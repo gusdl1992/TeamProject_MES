6b9c12ec727c299dfbfb382462996754e8ecd1ce
kulljam <adgrvet13@naver.com>
2024-04-01 15:35:34 +0900

    20240401 출력 엔티티 -> 디티오 변환

--- src/main/java/com/team1/model/repository/SurveyRepository.java
+++ src/main/java/com/team1/model/repository/SurveyRepository.java
@@ -17,5 +17,4 @@ public interface SurveyRepository extends JpaRepository<SurveyEntity,Integer> {
 
     @Query(value = "select * from survey as s join workplan as w on s.wno = w.wno where s.sno = :sno" , nativeQuery = true)
     Map<Object , Object> findBySurveyCheckCountSQL(int sno);
-
 }
