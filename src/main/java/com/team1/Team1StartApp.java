d7b7efeba9e8791e00d518406a6a844997ad87fa
kulljam <adgrvet13@naver.com>
2024-04-03 17:23:06 +0900

    20240403 하 시작 이다

--- src/main/java/com/team1/Team1StartApp.java
+++ src/main/java/com/team1/Team1StartApp.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class Team1StartApp {
     public static void main(String[] args) {
         SpringApplication.run( Team1StartApp.class );
+
         }
 
 }
