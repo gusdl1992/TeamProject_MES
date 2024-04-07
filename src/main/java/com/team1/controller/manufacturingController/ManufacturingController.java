b161bfe3759c7a44e4c1230b59cdae5bc3fc4ddb
HiYo0 <tmdgh034@naver.com>
2024-04-07 21:09:53 +0900

    2024.04.07 전승호 Manufacturing 정보 호출기능 추가

---
+++ src/main/java/com/team1/controller/manufacturingController/ManufacturingController.java
@@ -0,0 +1,42 @@
+package com.team1.controller.manufacturingController;
+
+import com.team1.model.dto.MaterialInputDto;
+import com.team1.model.dto.SurveyBDto;
+import com.team1.service.manufacturingService.ManufacturingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/manufacturing")
+@RestController
+public class ManufacturingController {//class star
+
+    @Autowired ManufacturingService manufacturingService;
+
+    //MaterialInput 정보 전부 가져오기
+    @GetMapping("/MaterialInput.do")
+    public List<MaterialInputDto> materialInputDtoList(){
+
+        return manufacturingService.materialInputDtoList();
+    }
+
+    // MaterialInput 클릭했을때 반환받는거
+    // mipno
+    @GetMapping("/MaterialInput/click.do")
+    public MaterialInputDto MaterialInputClickDo(@RequestParam int mipno){
+
+        System.out.println("mipno = " + mipno);
+
+        return manufacturingService.MaterialInputClickDo(mipno);
+    }
+
+    // survey 식별번호로 투입한 수량/내용 알아오기
+    @GetMapping("/surveyInfo.do")
+    public List<SurveyBDto> surveyInfoDo(@RequestParam int sno){
+        return manufacturingService.surveyInfoDo(sno);
+    }
+
+
+}//class end
