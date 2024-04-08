3a5287e2afc3b210f4c43ec9f757c4408ea2ec16
KDH <lococo0425@naver.com>
2024-04-08 18:35:40 +0900

    2024-04-08전승호 벌크제조 컨트롤 등록완료

---
+++ src/main/reactapp/src/component/content/manufacturing/MaterialinputList.jsx
@@ -0,0 +1,35 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom";
+import "./manufacturing.css"
+
+export default function MaterialinputList(){
+    
+    // 워크플랜
+    const [ materialinput , setMaterialinput] = useState([]);
+
+    useEffect(()=>{
+        axios.get("/manufacturing/MaterialInput.do")
+        .then((response)=>{
+            // console.log(response);
+            const result = response.data.map((materialinput)=>{return materialinput;})
+            setMaterialinput(result);
+        })
+    },[])
+    
+    return(<>
+        <div id="materialinputCssBox">
+        {materialinput.map((mip)=>{
+            return(
+                <div className="materialinputCss">
+                    <Link to={`/manufacturing/info?mipno=${mip.mipno}`}>
+                        <h4>투입내역{mip.mipno}</h4>
+                        <div>PlanNumber : {mip.workPlanDto.wno}</div>
+                        <div>등록일자 : {mip.cdate.split('T')[0].split('-')[0]}년 {mip.cdate.split('T')[0].split('-')[1]}월 {mip.cdate.split('T')[0].split('-')[2]}일</div>
+                    </Link>
+                </div>
+                )
+        })}
+        </div>
+    </>)
+}
