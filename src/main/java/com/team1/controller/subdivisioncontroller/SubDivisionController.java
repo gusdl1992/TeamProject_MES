8fa3f7d852c1060178ff62f0139f4d05a0b7a81d
me5290 <me5290@naver.com>
2024-04-09 16:54:44 +0900

    2024.04.09 16:54 이민형 소분 보고서 등록

--- src/main/java/com/team1/controller/subdivisioncontroller/SubDivisionController.java
+++ src/main/java/com/team1/controller/subdivisioncontroller/SubDivisionController.java
@@ -1,5 +1,6 @@
 package com.team1.controller.subdivisioncontroller;
 
+import com.team1.model.dto.ManufacturingDto.ManufacturingDto;
 import com.team1.model.dto.MaterialInputDto;
 import com.team1.model.dto.SubDivisionDto;
 import com.team1.service.memberserivce.MemberService;
@@ -19,7 +20,9 @@ public class SubDivisionController {
 
     // 소분 보고서 작성
     @PostMapping("/input/post.do")
-    public boolean doSubDivisionInputPost(@RequestParam int mfno , int failcount , int successcount){
+    public boolean doSubDivisionInputPost(@RequestParam int mfno , SubDivisionDto subDivisionDto){
+        int successcount = subDivisionDto.getSuccesscount();
+        int failcount = subDivisionDto.getFailcount();
         return subDivisionService.doSubDivisionInputPost(mfno,failcount,successcount);
     }
 
@@ -29,12 +32,20 @@ public class SubDivisionController {
         return subDivisionService.doSubDivisionAllinfoGet();
     }
 
-    // 벌크 정보 출력
+    // 벌크 모든 정보 출력
     @GetMapping("/manufacturing/get.do")
     public List<Object> doManufacturingAllinfoGet(){
         List<Object> result = subDivisionService.doManufacturingAllinfoGet();
         System.out.println("result = " + result);
         return result;
+    }
+
+    // 벌크 1개 정보 출력
+    @GetMapping("/manufacturing/one/get.do")
+    public ManufacturingDto doManufacturingOneInfoGet(@RequestParam int mfno){
+        ManufacturingDto manufacturingDto = subDivisionService.doManufacturingOneInfoGet(mfno);
+        System.out.println("벌크 1개 manufacturingDto = " + manufacturingDto);
+        return manufacturingDto;
     }
 
     // 품질 검사
