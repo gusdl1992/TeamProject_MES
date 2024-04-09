5f0e29a0ea019f5289aeebeb42c186a068754bef
kulljam <adgrvet13@naver.com>
2024-04-09 14:41:52 +0900

    20240409 취합 끝

--- src/main/java/com/team1/Team1StartApp.java
+++ src/main/java/com/team1/Team1StartApp.java
@@ -10,7 +10,6 @@ public class Team1StartApp {
     public static void main(String[] args) {
         SpringApplication.run( Team1StartApp.class );
         }
-
 }
 // 0327 18:00 총 취합 완료
 // 0328 15:00 총 취합 완료
