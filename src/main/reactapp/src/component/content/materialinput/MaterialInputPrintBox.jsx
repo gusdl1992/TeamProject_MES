898a766f4614433ba9608630e7290f36676ec502
geonwk95 <geonwk95@naver.com>
2024-04-15 02:16:49 +0900

    20240414 민형이와 함께하는 CSS

--- src/main/reactapp/src/component/content/materialinput/MaterialInputPrintBox.jsx
+++ src/main/reactapp/src/component/content/materialinput/MaterialInputPrintBox.jsx
@@ -1,6 +1,15 @@
 import axios from "axios";
 import { useContext, useEffect, useRef, useState } from "react";
 import { LoginInfoContext } from "../../Index";
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
 
 export default function MaterialInputPrintBox(props){
     let [material,setMaterial] = useState([]);
@@ -61,19 +70,83 @@ export default function MaterialInputPrintBox(props){
         setConfirmMemberName(e.target.value);
     }
     
+    const StyledTableCell = styled(TableCell)(({ theme }) => ({
+        [`&.${tableCellClasses.head}`]: {
+          backgroundColor: theme.palette.common.black,
+          color: theme.palette.common.white,
+        },
+        [`&.${tableCellClasses.body}`]: {
+          fontSize: 14,
+        },
+      }));
+      
+      const StyledTableRow = styled(TableRow)(({ theme }) => ({
+        '&:nth-of-type(odd)': {
+          backgroundColor: theme.palette.action.hover,
+        },
+        // hide last border
+        '&:last-child td, &:last-child th': {
+          border: 0,
+        },
+      }));
+      
+      function createData(name, calories, fat, carbs, protein) {
+        return { name, calories, fat, carbs, protein };
+      }
+      
+      const rows = [
+        createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+        createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+        createData('Eclair', 262, 16.0, 24, 6.0),
+        createData('Cupcake', 305, 3.7, 67, 4.3),
+        createData('Gingerbread', 356, 16.0, 49, 3.9),
+      ];
+
+
+
     return(
-        <div className="AcontentBox">
+        
+        <div className="AcontentBox">            
             <h3>목록</h3>
+            <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>생산 계획 번호</StyledTableCell>
+            <StyledTableCell align="right">담당자</StyledTableCell>
+            <StyledTableCell align="right">날짜</StyledTableCell>
+            <StyledTableCell align="right">상태</StyledTableCell>
+            <StyledTableCell align="right">비고</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {material.map((r) => (
+            
+            <StyledTableRow key={r.name}>
+              <StyledTableCell component="th" scope="row">
+                {r.workPlanDto.wno}
+              </StyledTableCell>
+              <StyledTableCell align="right">{r.inputmemberDto.mname}</StyledTableCell>
+              <StyledTableCell align="right">{r.udate.split('T')[0]}</StyledTableCell>
+              <StyledTableCell align="right">{
+                                                r.mipstate == 0 ? '검사대기' : r.mipstate == 1 ? '검사불합격' : r.mipstate == 2 ? '검사합격' : '-'
+                                            }</StyledTableCell>
+              <StyledTableCell align="right"><button onClick={()=>{document.querySelector('.modal'+r.mipno).style.display = 'block'}} type="button">상세보기</button></StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
             <table>
-            <colgroup>
+            {/* <colgroup>
                 <col width="10%"/>
                 <col width="20%"/>
                 <col width="30%"/>
                 <col width="15%"/>
                 <col width="25%"/>
-            </colgroup>
+            </colgroup> */}
                 <thead>
-                    <tr>
+                    <tr>                        
                         <th>
                             생산 계획 번호
                         </th>
@@ -147,8 +220,8 @@ export default function MaterialInputPrintBox(props){
                             )
                         })
                     }
-                </tbody>
-            </table>
+                </tbody>            
+                </table>
         </div>
     )
 }
