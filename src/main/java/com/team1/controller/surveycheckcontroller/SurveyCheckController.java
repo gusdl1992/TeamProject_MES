ee8b447165a07c7350e749ec509e417c2575f370
KDH <lococo0425@naver.com>
2024-04-15 13:20:59 +0900

    2024-04-12 전승호 각공정 버튼 / 인풋박스 CSS

--- src/main/java/com/team1/controller/surveycheckcontroller/SurveyCheckController.java
+++ src/main/java/com/team1/controller/surveycheckcontroller/SurveyCheckController.java
@@ -40,8 +40,8 @@ public class SurveyCheckController {
 
     // 2. 검사 완료 체크 시 검사 완료자 데이터 저장
     @PutMapping("/check/complete/put.do")
-    // 매개변수 state = 뷰에서 selctBox에서 선택한 값입니다. 0=검사대기 / 1=불합격 / 2=합격
-    public boolean surveyCheck(SurveyDto surveyDto , int state){ // 세션 저장 했다고 치고 작업
+    // 매개변수 state = 뷰에서 selctBox에서 선택한 값입니다. 0=검사대기 / 1=불합격  / 2=합격
+    public int surveyCheck(SurveyDto surveyDto , int state){ // 세션 저장 했다고 치고 작업
         int sno = surveyDto.getSno();
         int sstate = surveyDto.getSstate();
         System.out.println("state = " + state);
